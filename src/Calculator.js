f8b3770ade9557c91eed89456a6f64a2edfd01e2
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-26 17:07:46 -0700

    set up inital state of component

---
+++ src/Calculator.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Calculator extends Component {
+    constructor (props) {
+        super()
+        this.state = {
+          number: 0,
+          newNum: [],
+        }
+      } 
+    render(){
+        return (
+            <div className="container">
+                <h1>Add with React!</h1>
+                <div className="add">
+                    <input type="text"/>
+                    <span>+</span>
+                    <input type="text"/>
+                    <button>=</button>
+                    <h3>Addition results go here</h3>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Calculator
