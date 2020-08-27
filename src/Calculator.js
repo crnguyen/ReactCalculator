c23969829c70282a35ce5d14008c3ad2fa0373c6
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-27 08:59:30 -0700

    add jsx to calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,20 +4,41 @@ class Calculator extends Component {
     constructor (props) {
         super()
         this.state = {
-          number: 0,
-          newNum: [],
+          number: " ",
+          newNum: " ",
+          results: "results"
         }
       } 
+
+    setNum = (e, num) => {
+        this.setState({ [num]: e.target.value});
+      }
+
+    addNumber= e => {
+        let results = parseInt(this.state.num1)+parseInt(this.state.num2)
+        this.setState({results: results})
+    } 
+
     render(){
         return (
             <div className="container">
                 <h1>Add with React!</h1>
                 <div className="add">
-                    <input type="text"/>
-                    <span>+</span>
-                    <input type="text"/>
-                    <button>=</button>
-                    <h3>Addition results go here</h3>
+                <input type="number"
+                    name="num1"
+                    placeholder="first number"
+                    value={this.state.num1}
+                    onChange={ (e) => this.setNum(e, 'num1') }
+                    />      
+                <span>+</span>
+                <input type="number"
+                    name="num2"
+                    placeholder="second number"
+                    value={this.state.num2}
+                    onChange={ (e) => this.setNum(e, 'num2') }
+                    />
+                <button onClick={(e) => this.addNumber(e)}>=</button>
+                <h3>{this.state.results}</h3>
                 </div>
             </div>
         )
